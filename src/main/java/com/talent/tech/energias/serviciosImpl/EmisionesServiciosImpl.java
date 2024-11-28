d2490dab9665929ccea5bbb9cffb2305964ab4de
alopezm2504 <alopezm2504@gmail.com>
2024-11-28 17:30:22 -0500

    Fix configs

--- src/main/java/com/talent/tech/energias/serviciosImpl/EmisionesServiciosImpl.java
+++ src/main/java/com/talent/tech/energias/serviciosImpl/EmisionesServiciosImpl.java
@@ -44,7 +44,5 @@ public class EmisionesServiciosImpl implements EmisionesServicios {
         emisionesRepositorio.deleteById(id);
     }
 
-    public Integer obtenerPromedioEmisiones(){
-        return
-    }
+
 }
